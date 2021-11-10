93ce90f03eb704d9692c9456de26412de3413a21
wangtao <2521820396@qq.com>
2021-11-10 08:32:24 +0800

    ch1

---
+++ os/src/lang_items.rs
@@ -0,0 +1,18 @@
+// os/src/lang_items.rs
+use crate::sbi::shutdown;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
+}
---
+++ os/src/main.rs
@@ -0,0 +1,53 @@
+#![no_std]
+#![no_main]
+#![feature(global_asm)]
+#![feature(llvm_asm)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod comlog;
+
+use log::{*};
+global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata(); 
+        fn sbss();
+        fn ebss();      
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
+
+    comlog::init();
+    warn!("Deallocate frame: {:#x}", stext as usize);
+
+    info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    trace!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    error!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    panic!("Shutdown machine!");
+    
+}
